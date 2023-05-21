be5e620c2f47cdf7120d60e2500bd0b1810ddf93
bahadirmemis <sadikbahadir.memis@migrosonline.com>
2023-05-21 21:59:07 +0300

    Initial commit

---
+++ src/main/java/com/bahadirmemis/mailsender/MailController.java
@@ -0,0 +1,38 @@
+package com.bahadirmemis.mailsender;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Bahadır Memiş
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/api/v1/mails")
+public class MailController {
+
+  @GetMapping("/default-mail-address")
+  public String getMailAddress() {
+    return "sbahadirm@gmail.com";
+  }
+
+  @PostMapping
+  public boolean sendMail(@RequestBody MailSendRequestDto mailSendRequestDto){
+
+    System.out.println(mailSendRequestDto);
+
+    //for (int i = 0; i < 10; i++){
+    //
+    //  mailSendRequestDto.setBody(mailSendRequestDto.getBody() + "-" + i);
+    //
+    //  mailService.sendMail(mailSendRequestDto);
+    //}
+
+    return true;
+  }
+
+}
